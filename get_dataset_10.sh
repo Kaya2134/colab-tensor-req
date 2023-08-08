a6ade5e25387a04aed03d2319f017a8ec50a7e19
Kaya Kus <kayaalikus123@gmail.com>
2023-08-08 13:48:04 +0300

    ds

---
+++ get_dataset_10.sh
@@ -0,0 +1,3 @@
+apt install python-pip
+pip install gdown
+gdown https://drive.usercontent.google.com/download\?id\=1rjGdpzc7vJNfvwp5I0zNR9Wb5oBGZLEG\&export\=download\&authuser\=0\&confirm\=t\&uuid\=ea2e1a04-1212-42d6-aafc-83d11c2f82a4\&at\=AC2mKKQspwM1FRb9XoMviKoyriDI:1691482383599
